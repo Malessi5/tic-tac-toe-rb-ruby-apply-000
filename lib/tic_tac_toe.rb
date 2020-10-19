f451decf8905e2139244f49e45dd81f4dd758e5b
Mike Alessi <mikealessi5@gmail.com>
2020-10-19 18:26:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,8 +68,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index,token)
-token = current_player(board)
+def move(board, index, token)
+
   board[index] = token
 
 end
@@ -83,7 +83,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, token)
+    move(board, index, current_player(board))
     display_board(board)
 elsif !valid_move?(board,index)
   turn(board)
